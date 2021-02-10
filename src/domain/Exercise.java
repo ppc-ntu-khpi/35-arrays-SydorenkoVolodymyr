535579c9b4f16e67d819211516d83cd1a9e96d1c
SydorenkoVolodymyr <74309456+SydorenkoVolodymyr@users.noreply.github.com>
2021-02-10 21:22:26 +0200

    Add files via upload

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,35 @@
 package domain;
 
-public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
-    }
+
+ /**
+ * The class Final exercise implements exercise dod
+ * author Sydorenko
+ */ 
+public final class Exercise implements MyExercise {
+    private final int[] numbers;
+
+
+/** 
+ *
+ * Exercise
+ *
+     * @param length
+ */
+    public Exercise(final int length) { 
+
+        this.numbers = new int[length];}
+
+/** 
+ *
+ * Symmetric array
+ *
+ * @return int[]
+ */
+    public int[] symmetricArray() { 
+
+        final int length = this.numbers.length;
+        int count = (length % 2 == 0) ? 1 : 0;
+        for (int i = length / 2; i < length; i++) {
+            this.numbers[i] = this.numbers[length - i - 1] = count++; }
+        return this.numbers;}
 }
--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -1,12 +1,55 @@
 package test;
 
 import domain.Exercise;
+import java.util.Arrays;
 
+
+ /**
+ * The class Test result
+ * author Sydorenko
+ */ 
 public class TestResult {
 
-    public static void main(String[] args) {
 
-        int R=10;
-        System.out.println("Area of a circle for R="+R+"m is "+Exercise.Calculate(R)+"m"+'\u00B2');
+
+/** 
+ *
+ * Main
+ *
+ * @param args  the args
+ */
+    public static void main(String[] args) {  
+
+
+        final int length = 15;
+        final Exercise numberArray = new Exercise(length);
+        final int[] array = numberArray.symmetricArray();
+        System.out.println(Arrays.toString(array));
+    }
+
+    private static class Exercise2 {
+
+
+/** 
+ *
+ * Exercise2
+ *
+ * @return public
+ */
+        public Exercise2() { 
+
+        }
+
+
+/** 
+ *
+ * Symmetric array
+ *
+ * @return int[]
+ */
+        private int[] symmetricArray() { 
+
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
     }
 }
